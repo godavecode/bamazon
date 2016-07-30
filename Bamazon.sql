c792c6acf0f9d040f5b68310f2b6e1d26cb54ce4
David <David@Davids-MacBook-Pro-2.local>
2016-07-30 09:58:13 -0500

    upload

--- Bamazon.sql
+++ Bamazon.sql
@@ -1,2 +1,12 @@
-/*Create Database*/
+/*Create initial table*/
+CREATE DATABASE Bamazon;
+USE Bamazon;
 
+CREATE TABLE `Products` (
+  `ItemID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ProductName` varchar(50) DEFAULT NULL,
+  `DepartmentName` varchar(50) DEFAULT NULL,
+  `Price` decimal(10,0) DEFAULT NULL,
+  `StockQuantity` int(10) DEFAULT NULL,
+  PRIMARY KEY (`ItemID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,2 +1,100 @@
 //Dependencies
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var Table = require('cli-table');
+
+//Create Connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root", 
+    password: "", 
+    database: "Bamazon"
+});
+
+// connection.connect(function(err){
+// 	if(err) throw err;
+// 	console.log("connected as id " + connection.threadId);
+// })
+
+// connection.query('Select * From Products', function(error, response){
+// 	if(error) throw error;
+// 	console.log(response);
+// })
+
+
+//function display items
+function displayItems(){
+	connection.query('Select * From Products', function(error, response){
+		if(error){
+			console.log(error)
+		};
+		var displayTable = new Table({
+			//column headers are the item names
+			head: ['ItemID', 'ProductName', 'DepartmentName', 'Price', 'StockQuantity'],
+  			colWidths: [10, 20, 20, 10, 20]
+		});
+		// table is an Array, so you can push
+		for(var i = 0; i < response.length; i++){
+			displayTable.push([response[i].ItemID, response[i].ProductName, response[i].DepartmentName, 
+			response[i].Price, response[i].StockQuantity]); 
+		}
+		//console log to node screen
+		console.log(displayTable.toString());
+		userQuestions();	
+	});
+};
+
+function userQuestions(){
+	//ID of the product they would like to buy
+	inquirer.prompt([
+	{
+    	name: "ID",
+    	type: "input",
+    	message: "What item number you would like to purchase?"
+    },
+    {
+    	name: "Quantity",
+    	type: "input",
+    	message: "What is the quantity you would like to purchase?"
+    },
+
+	]).then(function(answers) {
+  		var purchaseID = answers.ID;
+  		var purchaseQty = answers.Quantity;
+  		itemsPurchased(purchaseID, purchaseQty);
+	});
+}
+
+function itemsPurchased(purchasedID, purchasedQuantity){
+	connection.query('SELECT * FROM Products WHERE ItemID = ' + purchasedID, function(error, response){
+		if(error){
+			console.log(error)
+		};
+		if (purchasedQuantity <= response[0].StockQuantity){
+			var yourCost = response[0].Price * purchasedQuantity;
+			//update user
+			console.log("Your total cost is: " + yourCost + " dollars.");
+			console.log("Thanks for your purchase.");
+			//update inventory
+			connection.query('UPDATE Products SET StockQuantity = StockQuantity - ' + purchasedQuantity + ' WHERE ItemID = ' + purchasedID);
+		}
+		else{
+			console.log("This product is out of stock.");
+		};
+	});
+};
+
+displayItems();
+
+
+
+
+
+
+
+
+
+
+
 
